47cff4dc7067dc1152c4cd39c276bf9c2c0d2fa1
Jason Stillwell <dragonfax@gmail.com>
2018-09-02 23:58:28 -0700

    .

--- renderer.go
+++ renderer.go
@@ -30,7 +30,6 @@ func initRender() {
 		}
 
 		n := sdl.NumJoysticks()
-		// var joystick *sdl.Joystick
 		if n < 1 {
 			panic(fmt.Sprintf("not enough joysticks %d", n))
 		}
@@ -40,18 +39,10 @@ func initRender() {
 		sdl.GameControllerEventState(sdl.ENABLE) // not used
 		controller = sdl.GameControllerOpen(0)
 
-		/* surface, err = window.GetSurface()
-		if err != nil {
-			panic(err)
-		} */
-
 	})
 }
 
 func drawRect() {
-	// surface.FillRect(nil, 0)
-	// rect := sdl.Rect{X: x, Y: y, W: 200, H: 200}
-	// surface.FillRect(&rect, 0xffff0000)
 
 	renderer.SetDrawColor(0, 0, 0, 255)
 	renderer.Clear()
@@ -62,7 +53,6 @@ func drawRect() {
 
 	drawMonsters()
 
-	// window.UpdateSurface()
 	renderer.Present()
 }
 
